38469e43d08ac56aafea178e2adc558e0d792da9
naeluh <naeluh@gmail.com>
2019-05-31 14:11:48 -0700

    fix func

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,3 @@
-const NetlifyAPI = require("netlify");
-const client = new NetlifyAPI(process.env.TOKEN);
-
-// Fetch
-async function getFunctions(client) {
-  try {
-    // request
-    const api = await client.listSites();
-    console.log(api);
-    // map out funcitions
-    let functions = api.map(r => {
-      console.log();
-      return r.url;
-    });
-    console.log(urls);
-    return urls;
-  } catch (e) {
-    console.log(e);
-    // handle error
-  }
+export default function getFunctions() {
+  console.log("dude");
 }
-
-export default getFunctions;
